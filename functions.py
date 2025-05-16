f3fa61dcb6c713d4fc60363a763fdee6f691d75f
AlinaRubila <angryagnstfanfic412@mail.ru>
2025-05-17 00:21:10 +0400

    init

---
+++ functions.py
@@ -0,0 +1,101 @@
+import random, configs, nltk
+
+from nltk.parse.corenlp import transform
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import LinearSVC
+
+
+def intent_classifier():
+    X_text = ["Дратути", "Покедова", "Чё как", "Представься", "Салют", "Бывай", "Как дела", "Как твоё имя", "Да"]
+    y = ['hello', 'goodbye', 'whatsup', 'name', 'hello', 'goodbye', 'whatsup', 'name', 'yes']
+    for intent, intent_data in configs.INTENTS_DICTIONARY.items():
+        for example in intent_data['examples']:
+            X_text.append(example)
+            y.append(intent)
+    vectorizer = TfidfVectorizer(analyzer='char', ngram_range=(3, 3))
+    X = vectorizer.fit_transform(X_text)
+    clf = LinearSVC()
+    clf.fit(X, y)
+    return clf, vectorizer
+
+def answer_classifier(dialogs):
+    X_text = []
+    y = []
+    for question, answer in dialogs:
+        X_text.append(question)
+        y.append(answer)
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(X_text)
+    clf = LogisticRegression()
+    clf.fit(X, y)
+    return clf, vectorizer
+
+def clear_str(r):
+    r = r.lower()
+    alphabet = configs.ALPHABET
+    r = [c for c in r if c in alphabet]
+    return ''.join(r)
+
+def load_dialogs():
+    with open('dialogues.txt', encoding="utf-8") as f:
+        content = f.read()
+    blocks = content.split("\n")
+    dialogs = []
+    for b in blocks:
+        dialog = b.split('\\')[:2]
+        if len(dialog) == 2:
+            pair = [clear_str(dialog[0]), dialog[1]]
+            if pair[0] and pair[1]:
+                dialogs.append(pair)
+    return dialogs
+
+def get_answer(replica, dialogs, clf, vectorizer):
+    replica = clear_str(replica)
+    replica_text = vectorizer.transform([replica]).toarray()[0]
+    answer = clf.predict([replica_text])[0]
+    for q, a in dialogs:
+        if a == answer:
+            q = clear_str(q)
+            distance = nltk.edit_distance(replica, q)
+            if distance / len (q) <= 0.7:
+                return answer
+
+def classify_intent(question, clf, vectorizer):
+    question = clear_str(question)
+    intent = clf.predict(vectorizer.transform([question]))[0]
+    for example in configs.INTENTS_DICTIONARY[intent]["examples"]:
+        example = clear_str(example)
+        distance = nltk.edit_distance(question, example)
+        if example and distance / len(example) <= 0.45:
+            return intent
+
+def answer_by_intent(intent):
+    image = None
+    if intent in configs.INTENTS_DICTIONARY:
+        responses_vars = configs.INTENTS_DICTIONARY[intent]["answers"]
+        if "theme_app" in configs.INTENTS_DICTIONARY[intent]:
+            if configs.INTENTS_DICTIONARY[intent]["theme_app"][0] == 'furniture_q_demand_advert':
+                image = f"images/{configs.INTENTS_DICTIONARY[intent]["theme_gen"]}.jpg"
+        return random.choice(responses_vars), image
+
+def failure():
+    phrases = configs.FAILURE
+    return random.choice(phrases)
+
+def bot(question):
+    dialogs = load_dialogs()
+    clf, vectorizer = intent_classifier()
+    clf1, vectorizer1 = answer_classifier(dialogs)
+    intent = classify_intent(question, clf, vectorizer)
+    if intent:
+        return answer_by_intent(intent)
+    answer = get_answer(question, dialogs, clf1, vectorizer1)
+    if answer: return answer, ""
+    else: return failure(), ""
+
+def orders_record(articule_number, fio, contact_info, address_data, payment):
+    f = open("orders.txt", "a")
+    order = f"ФИО: {fio}\nАртикул: {articule_number}\nКонтакты: {contact_info}\nАдрес доставки: {address_data}\nОплата {payment.lower()}\n\n"
+    f.writelines(order)
+    f.close()
---
+++ sofashopbot.py
@@ -0,0 +1,159 @@
+import functions, configs
+from telegram import Update, ReplyKeyboardMarkup,  ReplyKeyboardRemove
+from telegram.ext import Application, Updater, CommandHandler, MessageHandler, ContextTypes, filters, \
+    ConversationHandler
+
+token = configs.TOKEN
+ARTICULE, NAME, CONTACTS, ADDRESS, CASH_OR_CARD, CONFIRM = range(6)
+articule_number = ""
+fio = ""
+contact_info = ""
+address_data = ""
+payment = ""
+
+async def texting(update, context) -> None:
+    chat_id = update.message.chat_id
+    answer, image = functions.bot(update.message.text)
+    if image and image != "":
+        await context.bot.send_photo(chat_id=chat_id, photo=image, caption=answer)
+    else: await update.message.reply_text(answer)
+
+async def start(update, context) -> None:
+    await update.message.reply_text("Здравствуйте! Давайте начнём наш диалог\nВведите /catalog, чтобы ознакомиться с нашими товарами\nВведите /order, если хотите сделать заказ\nВведите /faq, чтобы получить ответы на интересующие вопросы\nВведите /help, чтобы увидеть список доступных команд)")
+
+async def help(update, context) -> None:
+    await update.message.reply_text("Нужна помощь? Введите /start, чтобы начать\nВведите /faq, чтобы получить ответы на интересующие вопросы\nВведите /contacts для получения контактной информации\nВведите /catalog, чтобы ознакомиться с нашими товарами\nВведите /cart, чтобы добавить предмет в корзину\nВведите /order, если хотите сделать заказ")
+
+async def faq(update, context) -> None:
+    await update.message.reply_text("1) Что это за компания? \nSuperSofaShop - лучший мебельный магазин во всей Москве. Мы работаем с 2022 года, однако наше качество ничуть не уступает маститым производителям!\n"+
+    "2) Почему именно мы?\nВ первую очередь, мы думаем о клиенте и предлагаем ему наилучшие варианты в соответствии с его потребностями. А во-вторых... все остальные)\n"+
+                                    "3) Как нас найти? \nНаш магазин находится по адресу: г. Москва, ул. Галушки Борисова, д. 6. Будем рады вас видеть!\n"+
+                                    "4) Как оформить у вас заказ?\nВы можете оформить заказ на нашем официальном сайте или здесь с помощью команды /order\n"+
+                                    "5) Как осуществляется доставка?\nМы доставляем мебель в любую точку Москвы или Подмосковья. Доставка бесплатная.\n"+
+                                    "6) Собираете ли вы мебель?\nМы осуществляем сборку мебели только по желанию клиента. Стоимость сборки зависит от сложности работ и высчитывается вместе с клиентом на предварительном созвоне.")
+
+async def info(update, context) -> None:
+    await update.message.reply_text("Телефон для связи: +799999999999\nНаша почта: sofa@shop.ru\nСайт sofashop.ru\nНаш адрес: г. Москва, ул. Галушки Борисова, д. 6")
+
+async def catalog(update, context) -> None:
+    cat = ""
+    for i in configs.CATOLOG:
+        cat += i + "\n"
+    await update.message.reply_text(f"{cat}\nЧтобы узнать о товаре больше, введите его наименование, а я вам выдам всю информацию!")
+
+async def order(update, context) -> int:
+    if articule_number != "":
+        await update.message.reply_text("Введите ФИО заказчика без запятых")
+        return NAME
+    await update.message.reply_text("Введите точное наименование артикула")
+    return ARTICULE
+
+async def articule(update, context) -> int:
+    global articule_number
+    articule_number = update.message.text
+    await update.message.reply_text("Введите ФИО заказчика без запятых")
+    return NAME
+
+async def name(update, context) -> int:
+    global fio
+    fio = update.message.text
+    await update.message.reply_text("Введите контактную информацию:\n-Либо номер телефона в формате +7**********\n-Либо адрес электронной почты в формате user@domain.ru")
+    return CONTACTS
+
+async def contacts(update, context) -> int:
+    global contact_info
+    contact_info = update.message.text
+    await update.message.reply_text("Укажите адрес доставки")
+    return ADDRESS
+
+async def address(update, context) -> int:
+    global address_data
+    address_data = update.message.text
+    reply_keyboard = [["Наличными", "Картой"]]
+    await update.message.reply_text("Укажите способ оплаты", reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Наличными или картой?"))
+    return CASH_OR_CARD
+
+async def cash_or_card(update, context) -> int:
+    global payment, articule_number, fio, contact_info, address_data
+    payment = update.message.text
+    reply_keyboard = [["ДА", "НЕТ"]]
+    await update.message.reply_text(f"Подтверждаете оформление заказа?\nДетали:\nФИО: {fio}\nАртикул: {articule_number}\nКонтакты: {contact_info}\nАдрес доставки: {address_data}\nОплата {payment.lower()}", reply_markup=ReplyKeyboardMarkup(
+        reply_keyboard, one_time_keyboard=True, input_field_placeholder="ДА/НЕТ"))
+    return CONFIRM
+
+async def confirmation(update, context) -> int:
+    yes_no = update.message.text
+    global articule_number, fio, contact_info, address_data, payment
+    if yes_no == "ДА":
+        functions.orders_record(articule_number, fio, contact_info, address_data, payment)
+        await update.message.reply_text("Благодарим за оформление! С вами свяжутся по деталям заказа)", reply_markup=ReplyKeyboardRemove())
+    else:
+        await update.message.reply_text("Вы отменили заказ!", reply_markup=ReplyKeyboardRemove())
+    articule_number = ""
+    fio = ""
+    contact_info = ""
+    address_data = ""
+    payment = ""
+    return ConversationHandler.END
+
+async def cancel(update, context) -> int:
+    global fio, articule_number, contact_info, address_data, payment
+    fio = ""
+    articule_number = ""
+    contact_info = ""
+    address_data = ""
+    payment = ""
+    await update.message.reply_text("Отмена заказа!")
+    return ConversationHandler.END
+
+async def cart(update, context) -> int:
+    await update.message.reply_text("Введите наименование товара, а я добавлю его в корзину!")
+    return 0
+
+async def add_to_cart(update, context) -> int:
+    a = update.message.text.lower()
+    a = a[0].upper() + a[1:]
+    global articule_number
+    if a in configs.GOODS:
+        if articule_number != "": articule_number += ", "
+        articule_number += a
+        await update.message.reply_text("Предмет успешно добавлен в корзину! Чтобы просмотреть корзину, введите /view")
+    else:
+        await update.message.reply_text("Извините, но такого предмета в каталоге нет(")
+    return ConversationHandler.END
+
+async def view_cart(update, context) -> None:
+    global articule_number
+    c = articule_number.split(", ")
+    n = ""
+    for i in c:
+        n += f"{i}\n"
+    await update.message.reply_text(f"Ваша корзина:\n{n}")
+
+def main():
+    print("Bot is started")
+    application = Application.builder().token(token).build()
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("catalog", catalog))
+    application.add_handler(CommandHandler("contacts", info))
+    application.add_handler(CommandHandler("faq", faq))
+    application.add_handler(CommandHandler("view", view_cart))
+    application.add_handler(ConversationHandler(entry_points=[CommandHandler("cart",cart)],
+                                                states={0: {MessageHandler(filters.TEXT & ~filters.COMMAND, add_to_cart)}},
+                                                fallbacks=[CommandHandler("add", add_to_cart)]))
+    application.add_handler(ConversationHandler(entry_points=[CommandHandler("order", order)],
+                                                states={ARTICULE: [MessageHandler(filters.TEXT & ~filters.COMMAND, articule)],
+                                                    NAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, name)],
+                                                    CONTACTS: [MessageHandler(filters.TEXT & ~filters.COMMAND, contacts)],
+                                                    ADDRESS: [MessageHandler(filters.TEXT & ~filters.COMMAND, address)],
+                                                    CASH_OR_CARD: [MessageHandler(filters.Regex("^(Наличными|Картой)$"),cash_or_card)],
+                                                    CONFIRM: [MessageHandler(filters.Regex("^(ДА|НЕТ)$"), confirmation)]},
+                                                fallbacks=[CommandHandler("cancel", cancel)]))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, texting))
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == "__main__":
+    main()
